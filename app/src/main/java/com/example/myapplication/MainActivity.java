f7c6968f55b2b5de8184ea12e0138b0358448768
inade <inadezda2003@gmail.com>
2023-02-25 18:39:34 +0300

    add log

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,36 +16,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
-
+        Log.i(TAG,"информация в onCreate"); // информация
+        Log.e(TAG,"ошибки в onCreate"); // ошибки
+        Log.w(TAG,"предупреждения в onCreate"); // предупреждения
+        Log.d(TAG,"отладка в onCreate"); // отладка
+        Log.v(TAG,"подробности в onCreate"); // подробности
     }
     @Override
     protected void onStart() {
         super.onStart();
         Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
-
+        Log.i(TAG,"информация в onStart"); // информация
+        Log.e(TAG,"ошибки в onStart"); // ошибки
+        Log.w(TAG,"предупреждения в onStart"); // предупреждения
+        Log.d(TAG,"отладка в onStart"); // отладка
+        Log.v(TAG,"подробности в onStart"); // подробности
     }
     @Override
     protected void onStop() {
         super.onStop();
         Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
-
+        Log.i(TAG,"информация в onStop");
+        Log.e(TAG,"ошибки в onStop"); // ошибки
+        Log.w(TAG,"предупреждения в onStop"); // предупреждения
+        Log.d(TAG,"отладка в onStop"); // отладка
+        Log.v(TAG,"подробности в onStop"); // подробности
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
-
+        Log.i(TAG,"информация в onDestroy");
+        Log.e(TAG,"ошибки в onDestroy"); // ошибки
+        Log.w(TAG,"предупреждения в onDestroy"); // предупреждения
+        Log.d(TAG,"отладка в onDestroy"); // отладка
+        Log.v(TAG,"подробности в onDestroy"); // подробности
     }
     @Override
     protected void onPause() {
         super.onPause();
         Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
-
+        Log.i(TAG,"информация в onPause");
+        Log.e(TAG,"ошибки в onPause"); // ошибки
+        Log.w(TAG,"предупреждения в onPause"); // предупреждения
+        Log.d(TAG,"отладка в onPause"); // отладка
+        Log.v(TAG,"подробности в onPause"); // подробности
     }
     @Override
     protected void onResume() {
         super.onResume();
         Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"информация в onResume");
+        Log.e(TAG,"ошибки в onResume"); // ошибки
+        Log.w(TAG,"предупреждения в onResume"); // предупреждения
+        Log.d(TAG,"отладка в onResume"); // отладка
+        Log.v(TAG,"подробности в onResume"); // подробности
+    }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"информация в onRestart");
+        Log.e(TAG,"ошибки в onRestart"); // ошибки
+        Log.w(TAG,"предупреждения в onRestart"); // предупреждения
+        Log.d(TAG,"отладка в onRestart"); // отладка
+        Log.v(TAG,"подробности в onRestart"); // подробности
     }
 }
